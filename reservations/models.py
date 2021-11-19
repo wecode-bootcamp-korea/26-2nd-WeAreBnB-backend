d6d05d58a78969f85bb0e7097a1eddd1f84666a7
jieun <jieunlee.me@gmail.com>
2021-11-19 11:09:08 +0900

    Modify: models.py 수정, db migration - Room, RoomImage, Reservation model 수정

--- reservations/models.py
+++ reservations/models.py
@@ -3,8 +3,8 @@ from core.models    import TimeStampModel
 
 class Reservation(TimeStampModel):
     reservation_code = models.CharField(max_length=200)
-    room             = models.ForeignKey('rooms.Room', on_delete=models.CASCADE)
     user             = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    room             = models.ForeignKey('rooms.Room', on_delete=models.CASCADE)
     check_in         = models.DateField()
     check_out        = models.DateField()
     days             = models.IntegerField(default=0)
