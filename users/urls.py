56ce4a7b25483f9530d83f3e3ec47e7014116239
jieun <jieunlee.me@gmail.com>
2021-11-21 17:54:26 +0900

    Add: User 프로필 이미지 AWS S3 파일업로드 구현

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from users.views import SignInView, SignUpView, KakaoLoginView
+from users.views import SignInView, SignUpView, KakaoLoginView, UserProfileView, UserProfileUploadView
 
+app_name = 'users'
 urlpatterns = [
-    path("/signup", SignUpView.as_view()),
-    path("/signin", SignInView.as_view()),
-    path("/kakaologin", KakaoLoginView.as_view()),
+    path("/signup"        , SignUpView.as_view()),
+    path("/signin"        , SignInView.as_view()),
+    path("/kakaologin"    , KakaoLoginView.as_view()),
+    path("/profile"       , UserProfileView.as_view()),
+    path("/profile-upload", UserProfileUploadView.as_view()),
 ]
