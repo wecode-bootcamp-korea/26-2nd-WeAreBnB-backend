bdc00b8a6eed992a1c45200031867435e75219e8
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2021-11-24 19:25:09 +0900

    mentor review

--- reservations/views.py
+++ reservations/views.py
@@ -8,16 +8,21 @@ from datetime                import datetime
 from rooms.models            import Room
 from users.models            import User
 
-class ReservationView(View):
+class ReservationsView(View):
+    @login_required
+    def get(self, request):
+        pass
+        #민정
+    
     @login_required
     def post(self, request):
         try:
-            data             = json.loads(request.body)
-            room             = data['room']
-            check_in         = datetime.strptime(data['check_in'], '%Y-%m-%d')
-            check_out        = datetime.strptime(data['check_out'], '%Y-%m-%d')
-            adult            = data['adult']
-            children         = data['children']
+            data      = json.loads(request.body)
+            room      = data['room']
+            check_in  = datetime.strptime(data['check_in'], '%Y-%m-%d')
+            check_out = datetime.strptime(data['check_out'], '%Y-%m-%d')
+            adult     = data['adult']
+            children  = data['children']
 
             if check_in <= datetime.now():
                 return JsonResponse({'message': 'INVALID_DATE'}, status=400)
@@ -43,24 +48,29 @@ class ReservationView(View):
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-class ReservationFixView(View):
+        except Room.DoesNotExist:
+            pass
+
+class ReservationView(View):
     @login_required
-    def put(self, request, reservation_code):
+    def patch(self, request, reservation_code):
         try:
             data        = json.loads(request.body)
-            user_id     = request.user.id
-            reservation = Reservation.objects.get(user_id = user_id, reservation_code = reservation_code)
+            user        = request.user
+            adult       = data['adult']
+            children    = data['children']
             check_in    = datetime.strptime(data['check_in'], '%Y-%m-%d')
             check_out   = datetime.strptime(data['check_out'], '%Y-%m-%d')
-
-            if data['adult'] + data['children'] > Room.objects.get(id=reservation.room_id).max_guest:
+            reservation = Reservation.objects.get(user = user, reservation_code = reservation_code)
+            
+            if adult + children > reservation.room.max_guest:
                 return JsonResponse({'message': 'EXCEED THE QUANTITY'}, status=400)
 
             reservation.check_in  = check_in
             reservation.check_out = check_out
             reservation.days      = (check_out - check_in).days
-            reservation.adult     = data['adult']
-            reservation.children  = data['children']
+            reservation.adult     = adult
+            reservation.children  = children
             reservation.save()
 
             return JsonResponse({'message': 'SUCCESS'}, status = 200)
@@ -77,8 +87,8 @@ class ReservationFixView(View):
     @login_required
     def delete(self, request, reservation_code):
         try:
-            user_id     = request.user.id
-            reservation = Reservation.objects.get(user_id = user_id, reservation_code = reservation_code)
+            user        = request.user
+            reservation = Reservation.objects.get(user = user, reservation_code = reservation_code)
 
             reservation.delete()
 
@@ -89,6 +99,3 @@ class ReservationFixView(View):
 
         except Reservation.DoesNotExist:
             return JsonResponse({'message': 'DOES_NOT_EXIST_RESERVATION'}, status = 400)
-
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
